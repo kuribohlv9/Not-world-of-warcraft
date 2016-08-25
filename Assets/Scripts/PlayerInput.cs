158f4d9b925868278ec51a92174974a2dd0c42fc
kuribohlv9 <kuribohlv9@gmail.com>
2016-08-26 01:00:08 +0200

    first commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput: MonoBehaviour {
+
+    public PlayerInputs Current;
+    public string PlayerNumber = "1";
+
+	// Use this for initialization
+	void Start () {
+        Current = new PlayerInputs();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        // Retrieve our current WASD or Arrow Key input
+        // Using GetAxisRaw removes any kind of gravity or filtering being applied to the input
+        // Ensuring that we are getting either -1, 0 or 1   
+        Vector2 moveInput = Vector2.zero;
+        float temptolerance = 0.1f;
+        if (Input.GetAxis("Horizontal") > temptolerance)
+            moveInput.x = 1;
+        else if (Input.GetAxis("Horizontal") < -temptolerance)
+            moveInput.x = -1;
+        if (Input.GetAxis("Vertical") > temptolerance)
+            moveInput.y = 1;
+        else if (Input.GetAxis("Vertical") < -temptolerance)
+            moveInput.y = -1;
+        
+        bool attackInput = Input.GetButtonDown("Fire1");
+        //bool jumpInput = Input.GetButtonDown("A Button" + PlayerNumber);
+        //bool continuousJumpInput = Input.GetButton("X Button" + PlayerNumber);
+        
+        //bool sticky = Input.GetButtonDown("X Button" + PlayerNumber);
+        //bool debug = Input.GetButton("X Button" + PlayerNumber);
+        //bool recall = Input.GetButtonDown("Y Button" + PlayerNumber);
+        //bool leftbumper = Input.GetButtonDown("Swap Left" + PlayerNumber);
+        //bool rightbumper = Input.GetButtonDown("Swap Right" + PlayerNumber);
+
+        Current = new PlayerInputs()
+        {
+            MoveInput = moveInput,
+            AttackInput = attackInput,
+            //ContinuousJumpInput = continuousJumpInput,
+            //Sticky = sticky,
+            //Debug = debug,
+            //LeftBumper = leftbumper,
+            //RightBumper = rightbumper,
+            //Recall = recall
+            
+        };
+	}
+}
+
+public struct PlayerInputs
+{
+    public Vector2 MoveInput;
+    public bool AttackInput;
+    public bool JumpInput;
+    public bool ContinuousJumpInput;
+    public bool Sticky;
+    public bool Debug;
+    public bool LeftBumper;
+    public bool RightBumper;
+    public bool Recall;
+}
---
+++ Assets/Scripts/PlayerMachine.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerMachine : MonoBehaviour {
+
+    private enum PlayerDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    private Vector2 MoveDirection;
+    private PlayerDirection Direction;
+    private bool Attack = false;
+    private float AttackTimer;
+
+    private PlayerInput input;
+    private Rigidbody2D rigidbody;
+    private Animator anim;
+    
+    public float MoveSpeed = 1.0f;
+    public float AttackSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Direction = PlayerDirection.Up;
+        input = GetComponent<PlayerInput>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveDirection = input.Current.MoveInput;
+        HandleDirection(MoveDirection);
+        HandleAttack();
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(MoveDirection.x * MoveSpeed, MoveDirection.y * MoveSpeed);
+    }
+
+    private void HandleDirection(Vector2 movement)
+    {
+        if (movement != Vector2.zero)
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+
+        if (movement.y == 1 && movement.x == 0)
+        {
+            Direction = PlayerDirection.Up;
+            anim.SetInteger("Direction", 1);
+            return;
+        }
+        if(movement.x == 1 && movement.y == 0)
+        {
+            Vector3 temp = transform.localScale;
+            temp.x = 1;
+            transform.localScale = temp;
+
+            Direction = PlayerDirection.Right;
+            anim.SetInteger("Direction", 2);
+            return;
+        }
+        else if (movement.y == -1 && movement.x == 0)
+        {
+            Direction = PlayerDirection.Down;
+            anim.SetInteger("Direction", 3);
+            return;
+        }
+        else if (movement.x == -1 && movement.y == 0)
+        {
+            Vector3 temp = transform.localScale;
+            temp.x = -1;
+            transform.localScale = temp;
+
+            Direction = PlayerDirection.Left;
+            anim.SetInteger("Direction", 4);
+            return;
+        }
+        else
+        {
+            anim.SetInteger("Direction", 0);
+        }
+    }
+    private void HandleAttack()
+    {
+        if (input.Current.AttackInput)
+        {
+            Attack = true;
+            AttackTimer = AttackSpeed;
+            anim.SetBool("IsAttacking", true);
+        }
+        if(Attack)
+        {
+            MoveDirection = Vector2.zero;
+            AttackTimer -= Time.deltaTime;
+            if(AttackTimer < 0)
+            {
+                Attack = false;
+                anim.SetBool("IsAttacking", false);
+            }
+        }
+    }
+}
